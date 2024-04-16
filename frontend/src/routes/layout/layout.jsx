f32e49c899913c56b00a71bdd85e746b0c1183e4
AshanJR01 <ashanjr@outlook.com>
2024-04-16 19:14:10 +0530

    layout route updated #01

--- frontend/src/routes/layout/layout.jsx
+++ frontend/src/routes/layout/layout.jsx
@@ -16,3 +16,20 @@ function Layout() {
     </div>
   );
 }
+function RequireAuth() {
+  const { currentUser } = useContext(AuthContext);
+
+  if (!currentUser) return <Navigate to="/login" />;
+  else {
+    return (
+      <div className="layout">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+    );
+  }
+}
