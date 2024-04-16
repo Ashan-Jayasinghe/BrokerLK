f4083ac44638cdb7fc14669891db9bb59978fe4d
AshanJR01 <ashanjr@outlook.com>
2024-04-16 19:28:18 +0530

    listpage updated #02

--- frontend/src/routes/listPage/listPage.jsx
+++ frontend/src/routes/listPage/listPage.jsx
@@ -8,37 +8,5 @@ import { Suspense } from "react";
 function ListPage() {
   const data = useLoaderData();
 
-  return (
-    <div className="listPage">
-      <div className="listContainer">
-        <div className="wrapper">
-          <Filter />
-          <Suspense fallback={<p>Loading...</p>}>
-            <Await
-              resolve={data.postResponse}
-              errorElement={<p>Error loading posts!</p>}
-            >
-              {(postResponse) =>
-                postResponse.data.map((post) => (
-                  <Card key={post.id} item={post} />
-                ))
-              }
-            </Await>
-          </Suspense>
-        </div>
-      </div>
-      <div className="mapContainer">
-        <Suspense fallback={<p>Loading...</p>}>
-          <Await
-            resolve={data.postResponse}
-            errorElement={<p>Error loading posts!</p>}
-          >
-            {(postResponse) => <Map items={postResponse.data} />}
-          </Await>
-        </Suspense>
-      </div>
-    </div>
-  );
-}
 
 export default ListPage;
